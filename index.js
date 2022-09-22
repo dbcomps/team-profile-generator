fd896d1981000da99d0ce2c6c594596a18590dd5
David Barraza <dbcomps@mac.com>
2022-09-21 20:06:10 -0500

    added questions for each employee and functions to prompt questions but will call later

--- index.js
+++ index.js
@@ -34,6 +34,53 @@ const employeeOptions = [
   },
 ];
 
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "Please enter the engineer's name: ",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please enter the engineer's employee ID number: ",
+    name: "employeeID",
+  },
+  {
+    type: "input",
+    message: "Please enter the engineer's email address: ",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please enter the engineer's GitHub username: ",
+    name: "githubUser",
+  }
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    message: "Please enter the intern's name: ",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please enter the intern's employee ID number: ",
+    name: "interID",
+  },
+  {
+    type: "input",
+    message: "Please enter the intern's email address: ",
+    name: "email",
+  },
+  {
+    type: "input",
+    message:
+      "Please enter the name of the intern's school: ",
+    name: "school",
+  },
+];
+
 function startPrompt() {
 	prompt(managerQuestions)
 	.then((data) => {
@@ -46,6 +93,20 @@ function addEmployee() {
 	prompt(employeeOptions)
 	.then((data) => {
 		console.log(data);
+	});
+};
+
+function addEngineer() {
+	prompt(engineerQuestions)
+	.then((data) => {
+		console.log(data)
+	});
+};
+
+function addIntern() {
+	prompt(internQuestions)
+	.then((data) => {
+		console.log(data)
 	});
 };
 
