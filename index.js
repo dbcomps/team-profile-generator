6c5fd768dfdae77902ee80f92f6aaaa87cbd6b2c
David Barraza <dbcomps@mac.com>
2022-09-21 19:29:52 -0500

    command line prompting for manager questions

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const { prompt } = require('inquirer');
 const fs = require('fs');
 
+// employee questions
+const managerQuestions = [
+  {
+    type: "input",
+    message: "Please enter the manager's name: ",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's employee ID number: ",
+    name: "employeeID",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's email address: ",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's office number: ",
+    name: "officeNumber",
+  }
+];
+
+function startPrompt() {
+	prompt(managerQuestions)
+	.then((data) => {
+		console.log(data);
+	});
+};
+
+startPrompt();
