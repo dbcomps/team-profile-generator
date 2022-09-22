74ddfe079ce98371d9ebcf60ce7e150ba78cc529
David Barraza <dbcomps@mac.com>
2022-09-21 20:20:07 -0500

    wrote failing Employee constructor test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/employee.js')
+
+console.log(new Employee());
+
+test('create an employee object', () => {
+	const employee = new Employee('Rick');
+	
+	expect(typeof employee).toBe('object');
+	expect(employee.name).toBe('Rick');
+});
+
+
